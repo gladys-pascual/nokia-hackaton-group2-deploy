4311dde6d68248252fe35eddcbc07c52596620ac
Matias <matias@gals-mbp.localdomain>
2020-09-05 15:07:45 +0200

    top banner done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,11 @@ import React from "react";
 const Banner = () => {
   return (
     <section className="banner-wrapper">
-      <h1 className="banner-title">Special situations, special needs.</h1>
+      <div className="banner-wrapper-background-img-container">
+        <img className="banner-wrapper-background-img" src="/images/nokia-top-banner.png" alt="Banner background" />
+      </div>
+      <h1 className="banner-title">Get things done faster.</h1>
+      
     </section>
   );
 };
